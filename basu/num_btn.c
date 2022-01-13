b19a12b29c01555c24186aec0cda58693d73cf58
basu021 <basudevrout2001@gmail.com>
2022-01-13 22:13:45 +0530

    .

---
+++ basu/num_btn.c
@@ -0,0 +1,24 @@
+// input : 5
+// output : 2 3 4
+
+// input : 8
+// output : 2 3 4 5 6 7
+
+// input : 11
+// output : 2 3 4 5 6 7 8 9 10
+
+#include <stdio.h>
+
+int main() {
+    int input;
+    printf("Enter a number : ");
+    scanf("%d", &input);
+
+    for(int i = 2; i<input; i++) {
+        printf("%d ", i); //value of i is printed
+
+        printf("%d ", input%i); //reminder of input/i is printed
+        
+        printf("\n"); // new line is printed
+    }
+}
